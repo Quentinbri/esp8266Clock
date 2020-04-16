8f0104f1626ffc05ac11acedc3a32d8f84d7f015
Quentin Briollant <quentin.bri@gmail.com>
2020-04-16 18:17:41 +0200

    Add configuration of the orientation

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,19 @@
 #include <WiFiUdp.h>
 #include <ArduinoJson.h>
 #include <TM1637Display.h>
+#include <string>
 
 // Server
 ESP8266WebServer webServer;
-AutoConnect      acPortal(webServer);
 WiFiClient       wifiClient;
+AutoConnect      acPortal(webServer);
+AutoConnectAux   acClockConfig;
+ACRadio(acClockConfigOrientation, {"Cable on left", "Cable on right"}, "Orientation", AC_Vertical, 1);
 
 // Time client
 WiFiUDP ntpUDP;
 NTPClient ntpClient(ntpUDP, "pool.ntp.org");
+String formattedTime;
 const char worldTimeApiURL[] = "http://worldtimeapi.org/api/ip";
 const size_t worldTimeApiJsonCapacity = JSON_OBJECT_SIZE(15) + 290;
 DynamicJsonDocument worldTimeApiJSON(worldTimeApiJsonCapacity);
@@ -25,10 +29,86 @@ bool offsetNeedUpdate = false;
 const int CLK = D6;
 const int DIO = D5;
 TM1637Display display(CLK, DIO);
+enum t_displayOrientation {
+  normal,
+  rotate
+};
+t_displayOrientation displayOrientation = normal;
+
+static const char AUX_CLOCKCONFIG[] PROGMEM = R"(
+{
+  "title": "Clock Config",
+  "uri": "/clock_config",
+  "menu": true,
+  "element": [
+    {
+      "name": "caption",
+      "type": "ACText",
+      "value": "Configuration of the clock",
+      "style": "font-family:Arial;font-weight:bold;text-align:center;margin-bottom:10px;color:DarkSlateBlue"
+    },
+    {
+      "name": "orientation",
+      "type": "ACSelect",
+      "label": "Select Orientation",
+      "option": ["Default","Inverted"],
+      "selected": 0
+    },
+    {
+      "name": "newline",
+      "type": "ACElement",
+      "value": "<br>"
+    },
+    {
+      "name": "apply",
+      "type": "ACSubmit",
+      "value": "Apply",
+      "uri": "/apply"
+    }
+  ]
+}
+)";
 
 void rootPage() {
-  char content[] = "Hello, world";
-  webServer.send(200, "text/plain", content);
+  String content =
+    "<html>"
+    "<head>"
+    "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
+    "<script type=\"text/javascript\">"
+    "setTimeout(\"location.reload()\", 1000);"
+    "</script>"
+    "</head>"
+    "<body>"
+    "<h2 align=\"center\" style=\"color:blue;margin:20px;\">esp8266Clock</h2>"
+    "<h3 align=\"center\" style=\"color:gray;margin:10px;\">{{DateTime}}</h3>"
+    "<p style=\"text-align:center;\">Reload the page to update the time.</p>"
+    "<p></p><p style=\"padding-top:15px;text-align:center\">" AUTOCONNECT_LINK(COG_24) "</p>"
+    "</body>"
+    "</html>";
+  String truncatedTime = formattedTime.substring(0,5);
+  content.replace("{{DateTime}}", truncatedTime);
+  webServer.send(200, "text/html", content);
+}
+
+void applyPage() {
+  // Retrieve the value of AutoConnectElement with arg function of WebServer class.
+  // Values are accessible with the element name.
+  //String  tz = webServer.arg("orientation");
+  String selectedOrientation = webServer.arg("orientation");
+
+  if(selectedOrientation == "Default")
+  {
+    displayOrientation = normal;
+  }
+  else if(selectedOrientation == "Inverted")
+  {
+    displayOrientation = rotate;
+  }
+
+  webServer.sendHeader("Location", String("http://") + webServer.client().localIP().toString() + String("/"));
+  webServer.send(302, "text/plain", "");
+  webServer.client().flush();
+  webServer.client().stop();
 }
 
 bool updateTimeOffset()
@@ -68,18 +148,63 @@ bool updateTimeOffset()
   return timeOffsetUpdated;
 }
 
+uint8_t rotateDigit(uint8_t digit) {
+  // XGFEDCBA
+  bool segX = SEG_DP & digit;
+  bool segG = SEG_G & digit;
+  bool segF = SEG_F & digit;
+  bool segE = SEG_E & digit;
+  bool segD = SEG_D & digit;
+  bool segC = SEG_C & digit;
+  bool segB = SEG_B & digit;
+  bool segA = SEG_A & digit;
+
+  uint8_t rotatedDigit = segX * SEG_DP + segG * SEG_G
+    + segC * SEG_F + segB * SEG_E
+    + segA * SEG_D + segF * SEG_C
+    + segE * SEG_B + segD * SEG_A;
+
+  return rotatedDigit;
+}
+
+void displayTime() {
+  int minutes = ntpClient.getMinutes();
+  int hours = ntpClient.getHours();
+
+  int hourDigits[4] = {hours/10, hours%10, minutes/10, minutes%10};
+  uint8_t hourDigitsSegments[4];
+  for(int i=0 ; i<4 ; i++)
+  {
+    if(displayOrientation == normal)
+    {
+      hourDigitsSegments[i] = display.encodeDigit((uint8_t)hourDigits[i]);
+      //hourDigitsSegments[i] = SEG_DP | hourDigitsSegments[i];
+    }
+    else if(displayOrientation == rotate)
+    {
+      hourDigitsSegments[3-i] = rotateDigit(display.encodeDigit((uint8_t)hourDigits[i]));
+      //hourDigitsSegments[3-i] = SEG_DP | hourDigitsSegments[3-i];
+    }
+  }
+  hourDigitsSegments[1] |= SEG_DP;
+  display.setSegments(hourDigitsSegments);
+}
+
 void setup() {
   Serial.begin(115200);
 
-  webServer.on("/home", rootPage);
+  webServer.on("/", rootPage);
+  webServer.on("/apply", applyPage);
 
   // AutoConnect
   AutoConnectConfig acConfig;
   acConfig.apid = "WiFi_Clock";
   acConfig.psk = "WiFi_Clock";
   acConfig.title = "WiFi Clock";
-  acConfig.homeUri = "/home";
+  //acConfig.homeUri = "/home";
   acPortal.config(acConfig);
+  acClockConfig.load(AUX_CLOCKCONFIG);
+  acPortal.join(acClockConfig);
 
   // Display
   display.setBrightness(0x0a); // Maximum brightness
@@ -103,6 +228,8 @@ void loop() {
 
   // Update NTP time
   ntpClient.update();
+  formattedTime = ntpClient.getFormattedTime();
+  Serial.println("Time: " + formattedTime);
 
   // Get and set the time offset the first minute of each hour
   if(offsetNeedUpdate && ntpClient.getMinutes() == 0)
@@ -114,10 +241,7 @@ void loop() {
     offsetNeedUpdate = true;
   }
   
-  Serial.println("Time: " + ntpClient.getFormattedTime());
-
-  int time = ntpClient.getHours() * 100 + ntpClient.getMinutes();
-  display.showNumberDecEx(time, 0b01000000);
+  displayTime();
 
   delay(1000);
 }
